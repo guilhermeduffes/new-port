90c13812998256113a9900e3eda6b5b487455617
guilhermeduffes <110461415+guilhermeduffes@users.noreply.github.com>
2023-01-31 09:58:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var icon = document.querySelector('.icon');
+var options = document.querySelector('.options');
+
+icon.addEventListener('click', function() {
+  options.classList.toggle('active');
+});
