35974b9e7d57117953e88e329160f4b618344d4f
guilherme <guiduffes@gmail.com>
2023-03-24 17:10:11 -0300

    End commit?

--- main.js
+++ main.js
@@ -1,72 +1,83 @@
-function buttonCV(){
-  window.open("assets/CV.pdf","_blank");
+document.getElementById("cards").onmousemove = e => {
+	for (const card of document.getElementsByClassName("card")) {
+		const rect = card.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top;
+
+		card.style.setProperty("--mouse-x", `${x}px`);
+		card.style.setProperty("--mouse-y", `${y}px`);
+	};
+}
+
+function buttonCV() {
+	window.open("assets/CV.pdf", "_blank");
 }
 
 var icon = document.querySelector('.icon');
 var options = document.querySelector('.options');
 
-icon.addEventListener('click', function() {
-  options.classList.toggle('active');
+icon.addEventListener('click', function () {
+	options.classList.toggle('active');
 });
 
 var idiomaSelecionado = document.querySelector(".idioma");
 var bandeira = document.querySelector(".bandeira");
 
-idiomaSelecionado.addEventListener("change", function() {
-  var idiomaselect = idiomaSelecionado.value;
+idiomaSelecionado.addEventListener("change", function () {
+	var idiomaselect = idiomaSelecionado.value;
 
-  switch (idiomaselect) {
-    case "pt":
-      bandeira.src = "pt.png";
-      window.location.assign("index.html");
-      break;
-    case "en":
-      bandeira.src = "en.png";
-      window.location.assign("index-en.html");
-      break;
-    case "es":
-      bandeira.src = "es.png";
-      window.location.assign("index-es.html");
-      break;
-  }
+	switch (idiomaselect) {
+		case "pt":
+			bandeira.src = "pt.png";
+			window.location.assign("index.html");
+			break;
+		case "en":
+			bandeira.src = "en.png";
+			window.location.assign("index-en.html");
+			break;
+		case "es":
+			bandeira.src = "es.png";
+			window.location.assign("index-es.html");
+			break;
+	}
 });
 
-function scrollParaTopo(){
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
+function scrollForHome() {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth"
+	});
+};
 
-function scrollAboutMe(){
-  const element = document.getElementById("sobre-mim2");
-  element.scrollIntoView({
-    behavior:"smooth"
-  });
-}
+function scrollAboutMe() {
+	const element = document.getElementById("sobre-mim2");
+	element.scrollIntoView({
+		behavior: "smooth"
+	});
+};
 
-function scrollParaProjetos(){
-  const element = document.getElementById("projetos");
-  element.scrollIntoView({
-    behavior:"smooth"
-  });
-}
+function scrollForProjects() {
+	const element = document.getElementById("projetos");
+	element.scrollIntoView({
+		behavior: "smooth"
+	});
+};
 
-document.getElementById("insta").addEventListener('click',function()  {
-  window.open("https://www.instagram.com/bx_crip");
-}); 
+function scrollForHabilities(){
+	const element = document.getElementById("pai-habilidades");
+	element.scrollIntoView({
+		behavior:"smooth"
+	});
+};
 
-document.getElementById("whatsapp").addEventListener('click',function()  {
-  window.open("https://wa.me/+5521971235386");
-});
+function linkForInsta(){
+	window.open("https://www.instagram.com/bx_crip");
+};
 
-document.getElementById("linkedin").addEventListener('click',function()  {
-  window.open("");
-});
+function linkForWhatsapp(){
+	window.open("https://wa.me/+5521971235386");
+};
 
-
-
-
-
-
-
+function linkForLinkedin(){
+	window.open("https://www.linkedin.com/in/guilherme-duffes-b83146240/");
+};
